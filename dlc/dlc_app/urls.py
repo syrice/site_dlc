5dac6c807f975263d6f30b6852dea9452b74ebb9
muhammadxoja-sayfidinov <muhammadxojasayfidinov986@gmail.com>
2021-03-03 21:19:53 +0500

    fix 1 (recept )

--- dlc/dlc_app/urls.py
+++ dlc/dlc_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
-urlpatterns=[
-    path('index/',views.index,name="index"),
+urlpatterns = [
+    path('index/', views.index, name="index"),
+    path('recept/', views.recept, name="recept"),
 ]
--- dlc/dlc_app/views.py
+++ dlc/dlc_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request,"index.html",{})
+    return render(request, 'index.html',{})
+def recept(request):
+    return render(request, 'receipe-post.html',{})
