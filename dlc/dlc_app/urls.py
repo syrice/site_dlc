2079261f0ca5b340fe5b8ab00420df442be42037
syrice <sar1k0moki@gmail.com>
2021-03-04 18:27:05 +0500

    hotfix(contact,blog-post,elemenst,index)

--- dlc/dlc_app/urls.py
+++ dlc/dlc_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('index/', views.index, name="index"),
     path('recept/', views.recept, name="recept"),
     path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('elements/', views.elements, name="elements"),
+    path('blogpost/', views.blogpost, name="blogpost"),
 ]
--- dlc/dlc_app/views.py
+++ dlc/dlc_app/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+def elements(request):
+    return render(request, 'elements.html', {})
 
+def contact(request):
+    return render(request, 'contact.html', {})
+
+def blogpost(request):
+    return render(request, 'blog-post.html', {})
 
 def recept(request):
     return render(request, 'receipe-post.html', {})
